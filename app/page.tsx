ac6f9549b0a201e2a1bc2e48984d27665639fb61
Yonatan Elias <yonatane504@gmail.com>
2023-11-06 10:22:33 +0300

    recoil installation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import styles from './page.module.scss'
 import Header from '@comp/Header'
 
+
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Header />  
-    </main>
+      <main className={styles.main}>
+        <Header />  
+      </main>
   )
 }
